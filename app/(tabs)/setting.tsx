3d4bf350ee68f6757754db4fd2bb06babd7db707
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-15 16:38:59 +0700

    REF: remove un use code

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -1,10 +1,5 @@
 import { Text } from "react-native";
 import SafeAreaShell from "@/components/SafeAreaShell";
-import { useEffect, useState } from "react";
-import { useSQLiteContext } from "expo-sqlite";
-import * as schema from "../../src/db/schema";
-import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite";
-import passwordInterface from "@/src/interfaces/passwordInterfaces";
 
 const Setting = () => {
   interface Row {
@@ -13,29 +8,9 @@ const Setting = () => {
     intValue: number;
   }
 
-  const db = useSQLiteContext();
-  const drizzleDb = drizzle(db, { schema });
-  const [data, setData] = useState<any[]>([]);
-  const passwords = useLiveQuery(drizzleDb.query.password.findMany());
-
-  useEffect(() => {
-    console.log("Data", data);
-    console.log("Password", passwords);
-  }, [data, passwords]);
-
   return (
     <SafeAreaShell>
-      <Text className="text-black">{"setting".repeat(10)}</Text>
-      {passwords.data.length > 0 ? (
-        passwords.data.map((item: passwordInterface) => (
-          <Text className="text-black" key={item.id}>
-            id: {item.id} category: {item.category} username: {item.username}{" "}
-            email: {item.email} password: {item.password} {item.pin}
-          </Text>
-        ))
-      ) : (
-        <Text className="text-black text-center">No data available</Text>
-      )}
+      <Text className="text-black">setting</Text>
     </SafeAreaShell>
   );
 };
