3d338dea07b6ef7cbbcd2ca47165d5659c7886e9
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-15 14:15:57 +0700

    FEAT: passwords slice for manage global state password include add, delete, update, and load more

---
+++ src/redux/features/passwordSlice.ts
@@ -0,0 +1,36 @@
+import passwordInterface from "@/src/interfaces/passwordInterfaces";
+import { createSlice } from "@reduxjs/toolkit";
+
+const passwordSlice = createSlice({
+  name: "passwords",
+  initialState: {
+    passwords: [] as passwordInterface[],
+  },
+  reducers: {
+    setPasswords: (state, action) => {
+      state.passwords = action.payload;
+    },
+    addNewPasswords: (state, action) => {
+      state.passwords = [...state.passwords, action.payload];
+    },
+    deletePasswords: (state, action) => {
+      state.passwords = state.passwords.filter(
+        (item) => item.id != action.payload
+      );
+    },
+    updatePasswords: (state, action) => {
+      state.passwords = state.passwords.map((item) => {
+        if (item.id == action.payload.id) {
+          return action.payload;
+        }
+        return item;
+      });
+    },
+    loadMorePasswords: (state, action) => {
+      state.passwords = [...state.passwords, ...action.payload];
+    },
+  },
+});
+
+export const { setPasswords } = passwordSlice.actions;
+export default passwordSlice.reducer;
