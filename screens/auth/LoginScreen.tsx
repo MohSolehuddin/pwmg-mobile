55406cf7cd8575978257f21f6b96292da3b0b60d
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-17 15:49:01 +0700

    FEAT: create login screen

--- screens/auth/LoginScreen.tsx
+++ screens/auth/LoginScreen.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import SafeAreaShell from "@/components/SafeAreaShell";
+import TextInputWithStyle from "@/components/TextInputWithStyle";
+import { useSQLiteContext } from "expo-sqlite";
+import { useRef } from "react";
+import { Text, View } from "react-native";
+import * as schema from "../../src/db/schema";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { useRouter } from "expo-router";
 
 const LoginScreen = () => {
+  const usernameRef = useRef<string>("");
+  const passwordRef = useRef<string>("");
+
+  const router = useRouter();
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db, { schema });
+  const handleOnLogin = async () => {
+    router.push("/(tabs)");
+  };
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaShell>
+      <View className="flex flex-1 items-center justify-center w-full h-full">
+        <Text className="text-3xl text-center font-extrabold text-mainBlue">
+          Login
+        </Text>
+        <TextInputWithStyle
+          placeholder="Username"
+          onChangeText={(text) => (usernameRef.current = text)}
+        />
+        <TextInputWithStyle
+          placeholder="Password"
+          onChangeText={(text) => (passwordRef.current = text)}
+        />
+        <CustomButton text="Login" onPress={handleOnLogin} />
+      </View>
+    </SafeAreaShell>
   );
 };
 
